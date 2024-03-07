efc6a363677a05d6104cc7e3632664c3dee8c6e8
trobert <trobert@42.fr>
2024-03-07 16:52:14 +0100

    fix getTextJustify function

--- api/src/controllers/controller.ts
+++ api/src/controllers/controller.ts
@@ -11,33 +11,69 @@ export const defaultHandler = (req: Request, res: Response) => {
   res.end();
 };
 
-//ca fait 80 caracteres, il y a des espaces et des mots, cela ne fait qu'une ligne
-const getTextJustify = (text: string) => {
-  if (text.length <= 80) return text;
-  const tab: string[] = text.split(' ');
-  let tab2: string[] = [];
+const splitTable = (table: string[]) => {
+  let splitedTab: string[] = [];
   let ret: string = '';
-
   let count: number = 0;
-  for (let i = 0; i < tab.length; i++) {
-    const word = tab[i];
+
+  for (let i = 0; i < table.length; i++) {
+    const word = table[i];
+
     count += word.length;
     if (count > 80) {
-      tab2.push(ret);
+      splitedTab.push(ret);
       ret = '';
       count = word.length;
     }
+    if (i === table.length - 1) {
+      splitedTab.push((ret += word));
+      break;
+    }
     ret += word + ' ';
     count++;
+  }
+  return splitedTab;
+};
 
-    if (i === tab.length - 1) {
-      tab2.push((ret += word));
+const getJustifiedTextString = (table: string[]) => {
+  let justifiedText: string = '';
+
+  table.forEach((line, index) => {
+    if (index !== 0) justifiedText += '\n';
+    let missingChar: number = 80 - line.length;
+    const words: string[] = line.split(/(\s+)/);
+    let middle: number = words.length / 2;
+    let i = 1;
+
+    while (missingChar > 0 && words.length > 1) {
+      middle = words.length / 2;
+      if (middle + i < words.length) {
+        words.splice(middle + i, 0, ' ');
+        missingChar--;
+      }
+      if (missingChar > 0 && middle - i >= 0) {
+        words.splice(middle - i, 0, ' ');
+        missingChar--;
+        i += 3;
+      }
     }
-  }
-  for (let index of tab2) {
-    console.log(`tab [] = ` + index);
-  }
-  return ret;
+    const string: string = words.join('');
+    justifiedText += string;
+  });
+  return justifiedText;
+};
+
+const getTextJustify = (text: string) => {
+  if (text.length <= 80) return text;
+  const spaceSplitedTab: string[] = text.split(' ');
+  let splitedTab: string[] = splitTable(spaceSplitedTab);
+
+  //   for (let line of splitedTab) {
+  //     const length: number = line.length;
+  //     console.log(`Line (${length}) = ` + line);
+  //   }
+
+  return getJustifiedTextString(splitedTab);
 };
 
 export const postTextJustifyHandler = (req: Request, res: Response) => {
@@ -55,8 +91,8 @@ export const postTextJustifyHandler = (req: Request, res: Response) => {
   }
   const justifiedText = getTextJustify(req.body);
   // check rate limit -> 402
-
   // add words/day in db
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.send(justifiedText);
