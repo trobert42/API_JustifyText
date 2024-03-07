240417995c6390c17ea97661394339eb97d53178
trobert <trobert@42.fr>
2024-03-07 12:12:40 +0100

    Adding gitignore to remove dist folder and node_modules

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,7 +5,7 @@ const hostname = process.env.HOSTNAME || 'localhost';
 const port = parseInt(process.env.PORT ?? '3000');
 const routes = require('./routes/routes.js');
 
-app.use(express.json());
+app.use(express.json()); //middleware
 app.use('/', routes);
 
 app.listen(port, () => {
