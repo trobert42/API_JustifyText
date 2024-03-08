37ff120ac7b74489d9ebd7233ea6479592a7bf7c
trobert <trobert@42.fr>
2024-03-08 16:47:46 +0100

    added regex test for input of email

--- api/src/controllers/controller.ts
+++ api/src/controllers/controller.ts
@@ -14,14 +14,11 @@ export const defaultHandler = (req: Request, res: Response) => {
   return res.status(200).json({ error: `API not found at ${req.url}` });
 };
 
-const arrangeLineByLength = (oldTable: string[]) => {
-  let wordsTab: string[] = [];
+const arrangeLinesByLength = (oldTable: string[]) => {
   let newTab: string[] = [];
-  console.log('oldTable length = ' + oldTable.length);
 
   for (let j = 0; j < oldTable.length; j++) {
-    wordsTab = oldTable[j].split(' ');
-    console.log('wordsTab length = ' + oldTable[j].length);
+    let wordsTab: string[] = oldTable[j].split(' ');
     let ret: string = '';
     let word: string = '';
     let count: number = 0;
@@ -40,28 +37,26 @@ const arrangeLineByLength = (oldTable: string[]) => {
       count++;
     }
   }
-
-  newTab.forEach((element) => {
-    console.log('Lines after rearranging -- [' + element + ']');
-  });
-  console.log('------------');
-
+  //   newTab.forEach((element) => {
+  //     console.log('Lines after rearranging -- [' + element + ']');
+  //   });
+  //   console.log('------------');
   return newTab;
 };
 
-const getJustifiedTextString = (table: string[]) => {
+const justifyLines = (table: string[]) => {
   let justifiedText: string = '';
 
   table.forEach((line, index) => {
     if (index !== 0) justifiedText += '\n';
-    let missingChar: number = max_length_text - line.length;
     const words: string[] = line.split(/(\s+)/);
     const length = words.length;
     let oneQuarter: number = Math.floor(length / 4);
+    let missingChar: number = max_length_text - line.length;
     let i = 0;
-    console.log(
-      `Line (${line.length}) missing : ${missingChar} [` + line + ']',
-    );
+    // console.log(
+    //   `Line (${line.length}) missing : ${missingChar} [` + line + ']',
+    // );
     while (missingChar > 0 && length > 1) {
       if (
         oneQuarter + i === Math.floor((length * 3) / 4) ||
@@ -73,21 +68,20 @@ const getJustifiedTextString = (table: string[]) => {
       i += 2;
     }
     const string: string = words.join('');
-    console.log(`New line = [${string}]`);
+    // console.log(`New line = [${string}]`);
     justifiedText += string;
   });
   return justifiedText;
 };
 
-const getTextJustify = (text: string) => {
+const getJustifiedTextString = (text: string) => {
   if (text.length <= max_length_text) return text;
   const lineSplitedTab: string[] = text.split('\n');
-  //   const spaceSplitedTab: string[] = text.split(/(?<=\n)|(?=\n)| /);
-  lineSplitedTab.forEach((element) => {
-    console.log('Lines before -- [' + element + ']');
-  });
-  console.log('------------');
-  return getJustifiedTextString(arrangeLineByLength(lineSplitedTab));
+  //   lineSplitedTab.forEach((element) => {
+  //     console.log('Lines before -- [' + element + ']');
+  //   });
+  //   console.log('------------');
+  return justifyLines(arrangeLinesByLength(lineSplitedTab));
 };
 
 const countWords = (line: string) => {
@@ -100,12 +94,12 @@ export const postTextJustifyHandler = async (
   res: Response,
 ) => {
   if (!req.is('text/plain')) {
-    return res.status(400).json({ error: `Content-Type wrong format` });
+    return res.status(400).json({ error: `Content-Type format not allowed` });
   }
   if (!req.body) {
     return res.status(400).json({ error: `Empty body` });
   }
-  const justifiedText = getTextJustify(req.body);
+  const justifiedText = getJustifiedTextString(req.body);
   const nbrWordsToAdd: number = countWords(justifiedText);
   const nbrActualWords: number = await User.getWordsCount(req.auth);
 
@@ -117,25 +111,23 @@ export const postTextJustifyHandler = async (
     });
   }
   updateUserWordsCount(req, res, nbrWordsToAdd, nbrActualWords);
+  console.log('Successfully justified text from post request!');
   res
     .status(200)
     .setHeader('Content-Type', 'text/plain')
     .send(justifiedText.toString());
 };
 
-const generateToken = (email: string) => {
-  const token = jwt.sign({ email: email }, process.env.JWT_SECRET);
-  return token;
-};
-
 export const postTokenHandler = (req: Request, res: Response) => {
   const email = req.body['email'];
-  if (!req.body || !email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!req.body || !email || !emailRegex.test(email)) {
     return res
       .status(400)
       .json({ error: `Bad request, missing body or email` });
   }
-  const token = generateToken(email);
+  const token = jwt.sign({ email: email }, process.env.JWT_SECRET);
   if (!token) {
     return res.status(500).json({ error: `Unable to retrieve token` });
   }
--- api/src/middleware/auth.ts
+++ api/src/middleware/auth.ts
@@ -11,14 +11,20 @@ module.exports = (
 ) => {
   try {
     if (req.headers && req.headers.authorization) {
+      if (req.headers.authorization.split(' ')[0] !== 'Bearer')
+        throw new Error();
       const token = req.headers.authorization.split(' ')[1];
+      if (!token) throw new Error();
       const payload = jwt.verify(token, process.env.JWT_SECRET);
       const foundUser = User.getUserFromEmail(payload.email);
       if (!payload.email || !foundUser) throw new Error();
       req.auth = payload.email;
       next();
+    } else {
+      throw new Error();
     }
   } catch (error) {
-    res.status(401).json({ error: `Wrong Token or user not found` });
+    res.status(401).json({ error: `Wrong Token, no token or user not found` });
   }
 };
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRvd2R0QHRhdGEuY29tIiwiaWF0IjoxNzA5OTExNTk0fQ.6HqlgFTTEM1c4X6zP2DxmX-ibXPbedtk64sgLwP6nTU
